90e0a1fa33c73c1f7dd504fe12e90b6b1ce8472e
EbbyFini <EbbyFini@gmx.ch>
2018-12-25 14:46:54 +0100

    Reset FixStatus if something going wrong

--- tracker/Sim808.h
+++ tracker/Sim808.h
@@ -65,6 +65,8 @@ MyGsmSim808::MyGsmSim808(Stream &stream)
 /** Read and parse a gps information from the sim808 modul in the own MyGps data class */
 bool MyGsmSim808::getGPS(MyGps &gps)
 {
+   gps.fixStatus = false;
+
    sendAT(GF("+CGNSINF"));
    if (waitResponse(GF(GSM_NL "+CGNSINF:")) != 1) {
       return false;
